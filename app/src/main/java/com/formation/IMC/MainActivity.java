d9beee91e3fa8d8b77c152d97d5f0c9be0dd361f
BerouSylla <syllaberou@gmail.com>
2023-08-05 17:21:53 +0100

    Initial Commit de Berou

---
+++ app/src/main/java/com/formation/IMC/MainActivity.java
@@ -0,0 +1,14 @@
+package com.formation.IMC;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
